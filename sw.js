f3ffe07e633e139e7408645d8a0e0781b9c9b497
napnip123-debug <napnip123@gmail.com>
2026-01-22 14:59:01 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,54 @@
-const CACHE_NAME = 'ppl-tracker-v1';const urlsToCache = [  '/',  '/index.html',  '/manifest.json',  'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'];// Install event - cache assetsself.addEventListener('install', (event) => {  event.waitUntil(    caches.open(CACHE_NAME)      .then((cache) => {        console.log('Opened cache');        return cache.addAll(urlsToCache);      })  );  self.skipWaiting();});// Activate event - clean up old cachesself.addEventListener('activate', (event) => {  event.waitUntil(    caches.keys().then((cacheNames) => {aa
+var CACHE_NAME = 'ppl-tracker-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
