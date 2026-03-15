77298659a4c3ff4132b96986a75ef0033fd56615
napnip123-debug <napnip123@gmail.com>
2026-03-15 21:53:45 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,29 @@
-var CACHE_NAME = 'gurkha-gains-v5';
+var CACHE_NAME = 'gurkha-gains-v7';
 var urlsToCache = [
   './',
   './index.html',
   './manifest.json'
 ];
 
+var fontUrls = [
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600&display=swap',
+  'https://fonts.gstatic.com/s/bebasneue/v14/JTUSjIg69CK48gW7PXoo9Wlhyw.woff2',
+  'https://fonts.gstatic.com/s/dmsans/v15/rP2tp2ywxg089UriI5-g4vlH9VoD8CmcqZG40F9JadbnoEwA.woff2',
+  'https://fonts.gstatic.com/s/jetbrainsmono/v18/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8yKxjPVmUsaaDhw.woff2'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Gurkha Gains Premium cache opened');
-      return cache.addAll(urlsToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(function(response) {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
+      return cache.addAll(urlsToCache).then(function() {
+        return Promise.allSettled(
+          fontUrls.map(url =>
+            fetch(url, { mode: 'cors' })
+              .then(response => { if (response.ok) return cache.put(url, response); })
+              .catch(() => {})
+          )
+        );
       });
-    }).catch(function() {
-      return caches.match('./index.html');
     })
   );
-});
+  self.skip
